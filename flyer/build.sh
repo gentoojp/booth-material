6a5626bac4a473cc763eecf03cd22a2ca3af14e2
Alice Ferrazzi <alice.ferrazzi@gmail.com>
2016-11-04 13:30:02 +0900

    Flyer before send for OSC 2016 tokyo fall

--- flyer/build.sh
+++ flyer/build.sh
@@ -1,3 +1,3 @@
 make clean && make
 dvipdfmx -f font.map gentoojp-flyer.dvi
-pdfcrop --margin '1.05832' gentoojp-flyer.pdf gentoojp-flyer.pdf
+#pdfcrop --margin '1.05832' gentoojp-flyer.pdf gentoojp-flyer.pdf
